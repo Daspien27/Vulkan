d5f8bd05dbd8b00398ee36cb6d7157c2871106c4
Amund Desmarais <amunddesmarais64@gmail.com>
2018-01-31 22:30:00 -0500

    Chapter 5 changes: Introduces Vulkan Instance

--- VulkanTutorial/HelloTriangleApplication.cpp
+++ VulkanTutorial/HelloTriangleApplication.cpp
@@ -1,10 +1,11 @@
 #include "HelloTriangleApplication.h"
 
 #include <functional>
-
-#define GLFW_INCLUDE_VULKAN //Includes <vulkan\vulkan.h> indicates that glfw is to load in Vulkan
-#include <GLFW/glfw3.h>
-
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
@@ -40,7 +41,87 @@ void HelloTriangleApplication::initWindow ()
 
 void HelloTriangleApplication::initVulkan ()
 {
+   createInstance ();
 }
+
+void HelloTriangleApplication::createInstance ()
+{
+   VkApplicationInfo appInfo = {};
+   appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+   appInfo.pApplicationName = "Hello Triangle";
+   appInfo.applicationVersion = VK_MAKE_VERSION (1, 0, 0);
+   appInfo.pEngineName = "No Engine";
+   appInfo.engineVersion = VK_MAKE_VERSION (1, 0, 0);
+   appInfo.apiVersion = VK_API_VERSION_1_0;
+
+   VkInstanceCreateInfo createInfo = {};
+   createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+   createInfo.pApplicationInfo = &appInfo;
+
+   unsigned int glfwExtensionCount = 0;
+   const char** glfwExtensions;
+
+   PrintSupportedExtensions ();
+
+   glfwExtensions = glfwGetRequiredInstanceExtensions (&glfwExtensionCount);
+
+   EnsureRequiredExtensionsAreSupported (glfwExtensions, glfwExtensionCount);
+   
+   createInfo.enabledExtensionCount = glfwExtensionCount;
+   createInfo.ppEnabledExtensionNames = glfwExtensions;
+   
+   createInfo.enabledLayerCount = 0;
+
+   if (vkCreateInstance (&createInfo, nullptr, &instance) != VK_SUCCESS)
+   {
+      throw std::runtime_error ("failed to create instance!");
+   }
+
+}
+
+void HelloTriangleApplication::PrintSupportedExtensions ()
+{
+   uint32_t extensionCount = 0;
+   vkEnumerateInstanceExtensionProperties (nullptr, &extensionCount, nullptr);
+   std::vector<VkExtensionProperties> extensions (extensionCount);
+   vkEnumerateInstanceExtensionProperties (nullptr, &extensionCount, extensions.data ());
+
+   std::cout << "available extensions: " << std::endl;
+
+   for (const auto& extension : extensions)
+   {
+      std::cout << "\t" << extension.extensionName << std::endl;
+   }
+}
+
+void HelloTriangleApplication::EnsureRequiredExtensionsAreSupported (const char ** RequiredExtensions, int NumRequiredExtensions)
+{
+   uint32_t extensionCount = 0;
+   vkEnumerateInstanceExtensionProperties (nullptr, &extensionCount, nullptr);
+   std::vector<VkExtensionProperties> extensions (extensionCount);
+   vkEnumerateInstanceExtensionProperties (nullptr, &extensionCount, extensions.data ());
+
+   std::vector<std::string> MissingExtensions;
+   for (unsigned int i = 0; i < NumRequiredExtensions; ++i)
+   {
+      auto& RequiredExtension = RequiredExtensions[i];
+      auto matchExtension = [&] (const VkExtensionProperties& extensionProp) { return strcmp (extensionProp.extensionName, RequiredExtension) == 0; };
+
+      auto findIfRequiredExtensionSupported = std::find_if (std::begin (extensions), std::end (extensions), matchExtension);
+
+      if (findIfRequiredExtensionSupported == std::end (extensions))
+      {
+         MissingExtensions.emplace_back (RequiredExtension);
+      }
+   }
+   if (!MissingExtensions.empty ())
+   {
+      throw std::runtime_error ("A required extension is not supported! Missing: \r\n\t"
+                                + std::accumulate (std::next (MissingExtensions.begin ()), MissingExtensions.end (), MissingExtensions[0],
+                                                   [] (std::string accum, std::string next) { return accum + "\r\n\t" + next; }));
+   }
+}
+
 
 void HelloTriangleApplication::mainLoop ()
 {
@@ -52,6 +133,8 @@ void HelloTriangleApplication::mainLoop ()
 
 void HelloTriangleApplication::cleanup ()
 {
+   vkDestroyInstance (instance, nullptr);
+
    glfwDestroyWindow (window);
 
    glfwTerminate ();
--- VulkanTutorial/HelloTriangleApplication.h
+++ VulkanTutorial/HelloTriangleApplication.h
@@ -1,6 +1,7 @@
 #pragma once
 
-struct GLFWwindow;
+#define GLFW_INCLUDE_VULKAN //Includes <vulkan\vulkan.h> indicates that glfw is to load in Vulkan
+#include <GLFW/glfw3.h>
 
 
 class HelloTriangleApplication
@@ -8,6 +9,7 @@ class HelloTriangleApplication
 private:
 
    GLFWwindow* window;
+   VkInstance instance;
 
 public:
    HelloTriangleApplication ();
@@ -16,7 +18,15 @@ public:
    void run ();
 private:
    void initWindow ();
+
    void initVulkan ();
+   void createInstance ();
+
+   static void PrintSupportedExtensions ();
+
+   static void EnsureRequiredExtensionsAreSupported (const char ** RequiredExtensions, int NumRequiredExtensions);
+
    void mainLoop ();
+
    void cleanup ();
 };
