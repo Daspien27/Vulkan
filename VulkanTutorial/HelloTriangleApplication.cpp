56a972933fa9be84d8a0cbdcea538387e75ecd97
Amund Desmarais <amunddesmarais64@gmail.com>
2018-02-07 21:18:59 -0500

    Chapter 28 changes: added Texture ImageView and Sampler

--- VulkanTutorial/HelloTriangleApplication.cpp
+++ VulkanTutorial/HelloTriangleApplication.cpp
@@ -299,6 +299,8 @@ void HelloTriangleApplication::initVulkan ()
    createFramebuffers ();
    createCommandPool ();
    createTextureImage ();
+   createTextureImageView ();
+   createTextureSampler ();
    createVertexBuffer ();
    createIndexBuffer ();
    createUniformBuffer ();
@@ -425,7 +427,10 @@ bool HelloTriangleApplication::isDeviceSuitable (VkPhysicalDevice device)
 
    }
 
-   return indices.isComplete () && extensionsSupported && swapChainAdequate;
+   VkPhysicalDeviceFeatures supportedFeatures;
+   vkGetPhysicalDeviceFeatures (device, &supportedFeatures);
+
+   return indices.isComplete () && extensionsSupported && swapChainAdequate && supportedFeatures.samplerAnisotropy;
 }
 
 QueueFamilyIndices HelloTriangleApplication::findQueueFamilies (VkPhysicalDevice device)
@@ -626,29 +631,37 @@ void HelloTriangleApplication::createImageViews ()
 
    for (size_t i = 0; i < swapChainImages.size (); ++i)
    {
-      VkImageViewCreateInfo createInfo = {};
-      createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
-      createInfo.image = swapChainImages[i];
-
-      createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
-      createInfo.format = swapChainImageFormat;
-
-      createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
-      createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
-      createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
-      createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
-
-      createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-      createInfo.subresourceRange.baseMipLevel = 0;
-      createInfo.subresourceRange.levelCount = 1;
-      createInfo.subresourceRange.baseArrayLayer = 0;
-      createInfo.subresourceRange.layerCount = 1;
-
-      if (vkCreateImageView (device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS)
-      {
-         throw std::runtime_error ("failed to create image views!");
-      }
+      swapChainImageViews[i] = createImageView (swapChainImages[i], swapChainImageFormat);
    }
+}
+
+VkImageView HelloTriangleApplication::createImageView (VkImage & image, VkFormat format)
+{
+   VkImageViewCreateInfo viewInfo = {};
+   viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+   viewInfo.image = image;
+
+   viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+   viewInfo.format = format;
+
+   viewInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+   viewInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+   viewInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+   viewInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+   viewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+   viewInfo.subresourceRange.baseMipLevel = 0;
+   viewInfo.subresourceRange.levelCount = 1;
+   viewInfo.subresourceRange.baseArrayLayer = 0;
+   viewInfo.subresourceRange.layerCount = 1;
+
+   VkImageView imageView;
+   if (vkCreateImageView (device, &viewInfo, nullptr, &imageView) != VK_SUCCESS)
+   {
+      throw std::runtime_error ("failed to create image view!");
+   }
+
+   return imageView;
 }
 
 void HelloTriangleApplication::createRenderPass ()
@@ -1010,7 +1023,8 @@ void HelloTriangleApplication::createLogicalDevice ()
    }
 
    VkPhysicalDeviceFeatures deviceFeatures = {};
-   
+   deviceFeatures.samplerAnisotropy = VK_TRUE;
+
    VkDeviceCreateInfo createInfo = {};
    createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
 
@@ -1502,6 +1516,43 @@ void HelloTriangleApplication::copyBufferToImage (VkBuffer buffer, VkImage image
    endSingleTimeCommands (commandBuffer, commandPoolTransfer, transferQueue);
 }
 
+void HelloTriangleApplication::createTextureImageView ()
+{
+   textureImageView = createImageView (textureImage, VK_FORMAT_R8G8B8A8_UNORM);
+}
+
+void HelloTriangleApplication::createTextureSampler ()
+{
+   VkSamplerCreateInfo samplerInfo = {};
+   samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+   samplerInfo.magFilter = VK_FILTER_LINEAR;
+   samplerInfo.minFilter = VK_FILTER_LINEAR;
+
+   samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+   samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+   samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+   
+   samplerInfo.anisotropyEnable = VK_TRUE;
+   samplerInfo.maxAnisotropy = 16;
+
+   samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
+   
+   samplerInfo.unnormalizedCoordinates = VK_FALSE;
+
+   samplerInfo.compareEnable = VK_FALSE;
+   samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
+
+   samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+   samplerInfo.mipLodBias = 0.0f;
+   samplerInfo.minLod = 0.0f;
+   samplerInfo.maxLod = 0.0f;
+
+   if (vkCreateSampler (device, &samplerInfo, nullptr, &textureSampler) != VK_SUCCESS)
+   {
+      throw std::runtime_error ("failed to create texture sampler!");
+   }
+}
+
 uint32_t HelloTriangleApplication::findMemoryType (uint32_t typeFilter, VkMemoryPropertyFlags properties)
 {
    VkPhysicalDeviceMemoryProperties memProperties;
@@ -1556,6 +1607,10 @@ void HelloTriangleApplication::endSingleTimeCommands (VkCommandBuffer commandBuf
 void HelloTriangleApplication::cleanup ()
 {
    cleanupSwapChain ();
+   
+   vkDestroySampler (device, textureSampler, nullptr);
+
+   vkDestroyImageView (device, textureImageView, nullptr);
 
    vkDestroyImage (device, textureImage, nullptr);
    vkFreeMemory (device, textureImageMemory, nullptr);
--- VulkanTutorial/HelloTriangleApplication.h
+++ VulkanTutorial/HelloTriangleApplication.h
@@ -106,6 +106,8 @@ private:
 
    VkImage textureImage;
    VkDeviceMemory textureImageMemory;
+   VkImageView textureImageView;
+   VkSampler textureSampler;
 
 public:
    HelloTriangleApplication ();
@@ -149,6 +151,8 @@ private:
 
    void createImageViews ();
 
+   VkImageView createImageView (VkImage & image, VkFormat format);
+
    void createRenderPass ();
 
    void createGraphicsPipeline ();
@@ -184,6 +188,9 @@ private:
    void createImage (uint32_t width, uint32_t height, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory);
    void transitionImageLayout (VkImage image, VkFormat format, VkImageLayout oldLayout, VkImageLayout newLayout);
    void copyBufferToImage (VkBuffer buffer, VkImage image, uint32_t width, uint32_t height);
+
+   void createTextureImageView ();
+   void createTextureSampler ();
 
    uint32_t findMemoryType (uint32_t typeFilter, VkMemoryPropertyFlags properties);
 
